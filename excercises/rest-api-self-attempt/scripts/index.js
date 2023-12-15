b2d31b1cde432acc933ae366f8fb3365c59a027f
Talib Smith <talibsmith77@gmail.com>
2023-12-15 09:22:44 -0500

    waifu project

--- excercises/rest-api-self-attempt/scripts/index.js
+++ excercises/rest-api-self-attempt/scripts/index.js
@@ -1,6 +1,18 @@
 "use strict"
+let maid_button = document.querySelector("#maidButton")
+let marin_kitagawa_button = document.querySelector("#marin-kitagawaButton")
+let mori_calliope_button = document.querySelector("#mori-calliopeButton")
+let raiden_shogun_button = document.querySelector("#raiden-shogunButton")
+let oppai_button = document.querySelector("#oppaiButton")
+let selfies_button = document.querySelector("#selfiesButton")
+let uniform_button = document.querySelector("#selfiesButton")
+
 
 async function getImages() {
-  let response = await fetch("https://api.waifu.im/search?included_tags=maid&height=>=2000")
-  let images = response.json()
+  let response = await fetch("https://api.waifu.im/tags")
+  let tags =  await response.json()
+    for (const tag of tags.versatile) {
+      console.log(tag)
+    }
 }
+getImages()
